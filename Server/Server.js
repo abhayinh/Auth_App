a6cc82164354073d6d846e0b7c682078e3a590ed
Abhaysinh Deshmukh <abhaysinhdeshmukh1@gmail.com>
2025-12-11 15:37:54 +0530

    Fix Vercel routing

--- Server/Server.js
+++ Server/Server.js
@@ -1,45 +1,48 @@
-import express from "express"
-import cors from "cors"
-import "dotenv/config.js"
-import cookieParser from "cookie-parser"
-import { connect } from "mongoose"
+import express from "express";
+import cors from "cors";
+import "dotenv/config.js";
+import cookieParser from "cookie-parser";
+import connect_db from "./Config/Mongodb.js";
 
+const port = process.env.PORT || 4000;
 
-const port =process.env.PORT || 4000
-connect_db()
+connect_db();
 
-import connect_db from "./Config/Mongodb.js"
-
-
-import authrouter from "./Routes/authrouter.js"
-import userroute from "./Routes/User_route.js"
+import authrouter from "./Routes/authrouter.js";
+import userroute from "./Routes/User_route.js";
 
 const allowedorigins = [
   "http://localhost:5173",
   "https://auth-app-gamma-five.vercel.app",  
 ];
 
-const app=express()
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-  origin: allowedorigins,
-  credentials: true
-}));
+const app = express();
 
+app.set("trust proxy", 1); // IMPORTANT FOR COOKIES
 
+app.use(express.json());
+app.use(cookieParser());
 
-// All Router end points 
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedorigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("CORS Not Allowed"), false);
+    },
+    credentials: true,
+  })
+);
 
-app.get("/",(req,res)=>{
-    res.send("Api working ")
-})
+app.get("/", (req, res) => {
+  res.send("Api working ");
+});
 
-app.use("/api/auth",authrouter)
-app.use("/api/user",userroute)
+app.use("/api/auth", authrouter);
+app.use("/api/user", userroute);
 
-
-
-app.listen(port,()=>{
-    console.log(`App is listen in ${port}`)
-})
+app.listen(port, () => {
+  console.log(`App is listen in ${port}`);
+});
