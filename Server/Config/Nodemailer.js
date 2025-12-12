f5d5f35f63756613048a286cb1a2a603b9d04f23
Abhaysinh Deshmukh <abhaysinhdeshmukh1@gmail.com>
2025-12-12 18:06:35 +0530

    update

--- Server/Config/Nodemailer.js
+++ Server/Config/Nodemailer.js
@@ -1,13 +1,9 @@
-import nodemailer from "nodemailer";
-
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   auth: {
     user: process.env.SMTP_USERNAME,
-    pass: process.env.SMTP_PASSWORD
-  }
+    pass: process.env.SMTP_PASSWORD,
+  },
 });
-
-export default transporter;
